34b52318570e2bdee3963e5b2a6c290de06fb5a0
AdmiN <AdmiN@DESKTOP-M101FJ5>
2023-09-08 15:00:00 +0530

    RAKESH

--- src/com/lbrce/CSEBProject/TeamLead.java
+++ src/com/lbrce/CSEBProject/TeamLead.java
@@ -11,5 +11,16 @@ public class TeamLead {
 		System.out.println("Its a fun method");
 
 	}
-
+	
+	
+	public static void cse5c5()
+	{
+		System.out.println("This is rakesh , and my number is 5c5 from cse B section \n Thank You");
+		
+	}
+	
+	
+	public static void cse5a6() {
+		System.out.println("I am bhart kumar \n thank u");
+	}
 }
