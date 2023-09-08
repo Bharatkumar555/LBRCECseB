86ecee3a56935d34c0d4b96e1cf53f3291678311
ITWS <ITWS@DESKTOP-SM7F9GB>
2023-09-08 14:15:37 +0530

    First Message

---
+++ src/com/lbrce/CSEBProject/TeamLead.java
@@ -0,0 +1,15 @@
+package com.lbrce.CSEBProject;
+
+public class TeamLead {
+
+	public static void main(String[] args) {
+		// TO
+		fun();
+
+	}
+	public static void fun() {
+		System.out.println("Its a fun method");
+
+	}
+
+}
